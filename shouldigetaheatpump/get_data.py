1b0d965d230e9f2b3961a7496476dea624a10f16
tomrutherford <tmrutherford@gmail.com>
2024-05-04 13:20:08 -0600

    realize there's an openmeteo library

--- shouldigetaheatpump/get_data.py
+++ shouldigetaheatpump/get_data.py
@@ -1,5 +1,12 @@
 from pendulum import Date
-# Testing
+import aiohttp
+
 
 async def get_weather_data(start: Date, end: Date):
-    '''Get historical weather data'''
+    """Get historical weather data"""
+
+    51.11516647256088, -114.06747997399614
+    async with aiohttp.ClientSession() as session:
+        async with session.get("http://httpbin.org/get") as resp:
+            print(resp.status)
+            print(await resp.text())
